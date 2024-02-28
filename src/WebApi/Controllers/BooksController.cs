675b1bc6769aea50340fe42a6835e25daa29f056
Renato Leite <hr2.renato@gmail.com>
2024-02-27 22:49:16 -0300

    Implementing controller

--- src/WebApi/Controllers/BooksController.cs
+++ src/WebApi/Controllers/BooksController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Application.UseCases.ListBooksUseCase;
+using Application.UseCases.ListBooksUseCase.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebApi.Controllers
 {
@@ -6,9 +8,130 @@ namespace WebApi.Controllers
     [ApiController]
     public class BooksController : Controller
     {
-        public IActionResult Index()
+        private IListBooksUseCase _useCase { get; set; }
+
+        public BooksController(IListBooksUseCase useCase)
         {
-            return View();
+            _useCase = useCase;
+        }
+
+        [HttpGet("authors/{author}")]
+        public async Task<IActionResult> ListBooksByAuthor(string author, int page, int items, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = await _useCase.ExecuteAsync(new ListBooksInput
+                {
+                    Term = author, Items = items,
+                    Page = page, Operation = Operation.ListBooksByAuthor
+                }, cancellationToken);
+
+                if (result == null)
+                    return NotFound();
+
+                return Ok(result);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet("isbn/{isbnCode}")]
+        public async Task<IActionResult> ListBooksByISBN(string isbnCode, int page, int items, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = await _useCase.ExecuteAsync(new ListBooksInput
+                {
+                    Term = isbnCode,
+                    Items = items,
+                    Page = page,
+                    Operation = Operation.ListBooksByISBN
+                }, cancellationToken);
+
+                if (result == null)
+                    return NotFound();
+
+                return Ok(result);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet("users/{userId}/owned")]
+        public async Task<IActionResult> ListOwnedBooksByUser(int userId, int page, int items, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = await _useCase.ExecuteAsync(new ListBooksInput
+                {
+                    UserId = userId,
+                    Items = items,
+                    Page = page,
+                    Operation = Operation.ListOwnedBooksByUserId
+                }, cancellationToken);
+
+                if (result == null)
+                    return NotFound();
+
+                return Ok(result);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet("users/{userId}/loved")]
+        public async Task<IActionResult> ListLovedBooksByUser(int userId, int page, int items, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = await _useCase.ExecuteAsync(new ListBooksInput
+                {
+                    UserId = userId,
+                    Items = items,
+                    Page = page,
+                    Operation = Operation.ListLovedBooksByUserId
+                }, cancellationToken);
+
+                if (result == null)
+                    return NotFound();
+
+                return Ok(result);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+
+        [HttpGet("users/{userId}/want-read")]
+        public async Task<IActionResult> ListWantToReadBooksByUser(int userId, int page, int items, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = await _useCase.ExecuteAsync(new ListBooksInput
+                {
+                    UserId = userId,
+                    Items = items,
+                    Page = page,
+                    Operation = Operation.ListWantToReadBooksByUserId
+                }, cancellationToken);
+
+                if (result == null)
+                    return NotFound();
+
+                return Ok(result);
+            }
+            catch
+            {
+                return BadRequest();
+            }
         }
     }
 }
